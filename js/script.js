be9013314c6bd44b98ec24cd1b4e48d7950401a2
Jyotimoykathar <jyotimoykatharon@gmail.com>
2022-01-19 17:26:43 +0530

    animation19Jan22

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+const textEl = document.querySelector(".auto-text");
+const text = "OUR FEATURE PRODUCTS";
+let idx = 1;
+writeText();
+function writeText() {
+  textEl.innerText = text.slice(0, idx);
+  idx++;
+  if (idx > text.length) {
+    idx = 1;
+  }
+  setTimeout(writeText, 150);
+}
